239514f863dc49ceb5dd18c714efd0a5ced0bdb0
Aykhan <ayxanshikhaliyevuv@gmail.com>
2025-07-02 17:28:33 +0400

    Revert "attempt again"
    This reverts commit af255546af4a956c2be2d13edc73afbfac7ade94.

--- src/providers/all.ts
+++ src/providers/all.ts
@@ -30,7 +30,13 @@ import {
   oneServerPrimeboxEmbed,
   oneServerVidsrcsuEmbed,
 } from './embeds/1server';
-import { autoembedScraper as autoembedEmbedScraper } from './embeds/autoembed';
+import {
+  autoembedBengaliScraper,
+  autoembedEnglishScraper,
+  autoembedHindiScraper,
+  autoembedTamilScraper,
+  autoembedTeluguScraper,
+} from './embeds/autoembed';
 import { cinemaosEmbeds } from './embeds/cinemaos';
 import { closeLoadScraper } from './embeds/closeload';
 import {
@@ -162,7 +168,11 @@ export function gatherAllEmbeds(): Array<Embed> {
     warezcdnembedHlsScraper,
     warezcdnembedMp4Scraper,
     warezPlayerScraper,
-    autoembedEmbedScraper,
+    autoembedEnglishScraper,
+    autoembedHindiScraper,
+    autoembedBengaliScraper,
+    autoembedTamilScraper,
+    autoembedTeluguScraper,
     turbovidScraper,
     mp4hydraServer1Scraper,
     mp4hydraServer2Scraper,
