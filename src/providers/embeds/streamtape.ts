6ef2ff5065a414c27a9816d50471a261a0bf56f3
Aykhan <ayxanshikhaliyevuv@gmail.com>
2025-07-02 15:57:05 +0400

    Update streamtape.ts

--- src/providers/embeds/streamtape.ts
+++ src/providers/embeds/streamtape.ts
@@ -1,5 +1,6 @@
 import { flags } from '@/entrypoint/utils/targets';
 import { makeEmbed } from '@/providers/base';
+import { createM3U8ProxyUrl } from '@/utils/proxy';
 
 const providers = [
   {
@@ -38,18 +39,12 @@ function embed(provider: { id: string; name: string; rank: number }) {
         stream: [
           {
             id: 'primary',
-            type: 'file',
+            type: 'hls',
+            playlist: createM3U8ProxyUrl(url, {
+              Referer: 'https://streamtape.com',
+            }),
             flags: [flags.CORS_ALLOWED, flags.IP_LOCKED],
             captions: [],
-            qualities: {
-              unknown: {
-                type: 'mp4',
-                url,
-              },
-            },
-            headers: {
-              Referer: 'https://streamtape.com',
-            },
           },
         ],
       };
