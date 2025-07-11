d43b959c80ad261157a0ecddcccf9735b12b72bb
Aykhan <ayxanshikhaliyevuv@gmail.com>
2025-07-11 13:21:58 +0400

    update zoechip

--- src/providers/all.ts
+++ src/providers/all.ts
@@ -19,6 +19,7 @@ import { mp4hydraScraper } from '@/providers/sources/mp4hydra';
 import { tugaflixScraper } from '@/providers/sources/tugaflix';
 import { vidsrcScraper } from '@/providers/sources/vidsrc';
 import { vidsrcsuScraper } from '@/providers/sources/vidsrcsu';
+import { zoechipScraper } from '@/providers/sources/zoechip';
 
 import {
   oneServerAnimepaheEmbed,
@@ -153,6 +154,7 @@ export function gatherAllSources(): Array<Sourcerer> {
     cinemaosScraper,
     hollymoviehdScraper,
     oneroomScraper,
+    zoechipScraper,
   ];
 }
 
