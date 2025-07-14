68239a1d97bb8d966d30be0e3dcc2f1f7a2f251d
Aykhan <ayxanshikhaliyevuv@gmail.com>
2025-07-14 13:59:01 +0400

    Revert "add aether"
    This reverts commit 60faa1c7e05d1904864373634633bdb7fbda704e.

--- src/providers/all.ts
+++ src/providers/all.ts
@@ -9,7 +9,6 @@ import {
 import { mixdropScraper } from '@/providers/embeds/mixdrop';
 import { turbovidScraper } from '@/providers/embeds/turbovid';
 import { upcloudScraper } from '@/providers/embeds/upcloud';
-import { aetherScraper } from '@/providers/sources/aether';
 import { autoembedScraper } from '@/providers/sources/autoembed';
 import { catflixScraper } from '@/providers/sources/catflix';
 import { ee3Scraper } from '@/providers/sources/ee3';
@@ -122,7 +121,6 @@ import { xprimeScraper } from './sources/xprime';
 export function gatherAllSources(): Array<Sourcerer> {
   // all sources are gathered here
   return [
-    aetherScraper,
     cuevana3Scraper,
     catflixScraper,
     ridooMoviesScraper,
