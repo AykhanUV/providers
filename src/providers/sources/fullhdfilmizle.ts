0b197657aacefd84ab7f16874c9ec6cd233d9b5b
Aykhan <ayxanshikhaliyevuv@gmail.com>
2025-07-15 02:05:33 +0400

    update agents

--- src/providers/sources/fullhdfilmizle.ts
+++ src/providers/sources/fullhdfilmizle.ts
@@ -62,9 +62,13 @@ async function comboScraper(ctx: MovieScrapeContext | ShowScrapeContext): Promis
 
   const playerPage = await ctx.proxiedFetcher<string>(playerUrl, {
     headers: {
-      Referer: mediaLink,
-      'User-Agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+      Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Language': 'en-US,en;q=0.5',
+      'Sec-Fetch-Dest': 'iframe',
+      'Sec-Fetch-Mode': 'navigate',
+      'Sec-Fetch-Site': 'cross-site',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0',
+      Referer: 'https://www.fullhdfilmizlesene.so/',
     },
   });
 
