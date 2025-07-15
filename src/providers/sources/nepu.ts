85de3be4c3979fdd9e35dfd6122db3df7065c825
Aykhan <ayxanshikhaliyevuv@gmail.com>
2025-07-15 23:25:55 +0400

    disable nepu

--- src/providers/sources/nepu.ts
+++ src/providers/sources/nepu.ts
@@ -39,6 +39,7 @@ export const nepuScraper = makeSourcerer({
   id: 'nepu',
   name: 'Nepu 💧',
   rank: 201,
+  disabled: true,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: scrape,
   scrapeShow: scrape,
