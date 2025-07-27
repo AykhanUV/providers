501159fa9f3faccb597e07ffd5b087c6b08ec3de
Aykhan <ayxanshikhaliyevuv@gmail.com>
2025-07-27 18:30:31 +0400

    make ekseni non-extension

--- src/providers/sources/filmekseni.ts
+++ src/providers/sources/filmekseni.ts
@@ -1,5 +1,6 @@
 import { load } from 'cheerio';
 
+import { flags } from '@/entrypoint/utils/targets';
 import { SourcererOutput, makeSourcerer } from '@/providers/base';
 import { MovieScrapeContext, ShowScrapeContext } from '@/utils/context';
 import { NotFoundError } from '@/utils/errors';
@@ -64,7 +65,7 @@ export const filmekseniScraper = makeSourcerer({
   id: 'filmekseni',
   name: 'Ekseni ♠️(Turkish)',
   rank: 181,
-  flags: [],
+  flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
   scrapeShow: comboScraper,
 });
