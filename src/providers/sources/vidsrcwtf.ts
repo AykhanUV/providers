5e43b693fcd18cf74da8dc3d6739c2726f48ef54
Aykhan <ayxanshikhaliyevuv@gmail.com>
2025-08-05 11:23:57 +0400

    add shows to vidsrc.wtf

--- src/providers/sources/vidsrcwtf.ts
+++ src/providers/sources/vidsrcwtf.ts
@@ -1,3 +1,4 @@
+import type { ShowMedia } from '@/entrypoint/utils/media';
 import { SourcererOutput, makeSourcerer } from '@/providers/base';
 import { MovieScrapeContext, ShowScrapeContext } from '@/utils/context';
 import { NotFoundError } from '@/utils/errors';
@@ -15,25 +16,51 @@ async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promis
     },
   });
 
-  const playerConfigMatch = playerPage.match(/let pc = ({.*});/s);
+  let playerConfigMatch = playerPage.match(/let pc = ({.*});/s);
+  if (!playerConfigMatch) {
+    playerConfigMatch = playerPage.match(/new HDVBPlayer\((.*)\);/s);
+  }
   if (!playerConfigMatch) throw new Error('Failed to find player config');
   const playerConfig = JSON.parse(playerConfigMatch[1]);
 
-  const sources = await ctx.proxiedFetcher<any>(playerConfig.file, {
-    method: 'POST',
-    headers: {
-      'X-CSRF-TOKEN': playerConfig.key,
-      Referer: `${mainUrl}/`,
-      Origin: mainUrl,
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:141.0) Gecko/20100101 Firefox/141.0',
-      'Content-type': 'application/x-www-form-urlencoded',
+  const sources = await ctx.proxiedFetcher<any>(
+    playerConfig.file.startsWith('/playlist') ? playerConfig.file : `/playlist/${playerConfig.file}`,
+    {
+      baseUrl: mainUrl,
+      method: 'POST',
+      headers: {
+        'X-CSRF-TOKEN': playerConfig.key,
+        Referer: `${mainUrl}/`,
+        Origin: mainUrl,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:141.0) Gecko/20100101 Firefox/141.0',
+        'Content-type': 'application/x-www-form-urlencoded',
+      },
     },
-  });
+  );
 
-  const englishSource = sources[1];
-  if (!englishSource) throw new Error('No english source found');
+  let finalFilePath: string;
 
-  const finalFilePath = englishSource.file.startsWith('~') ? englishSource.file.substring(1) : englishSource.file;
+  if (ctx.media.type === 'movie') {
+    const englishSource = sources[1];
+    if (!englishSource) throw new Error('No english source found');
+    finalFilePath = englishSource.file;
+  } else {
+    const media = ctx.media as ShowMedia;
+    const season = sources.find((s: any) => s.title === `Season ${media.season.number}`);
+    if (!season) throw new NotFoundError('Season not found');
+
+    const episode = season.folder.find((e: any) => e.episode === `${media.episode.number}`);
+    if (!episode) throw new NotFoundError('Episode not found');
+
+    const englishSource = episode.folder.find((f: any) => f.title === 'English');
+    if (!englishSource) throw new NotFoundError('No english source found for episode');
+    finalFilePath = englishSource.file;
+  }
+
+  if (finalFilePath.startsWith('~')) {
+    finalFilePath = finalFilePath.substring(1);
+  }
+
   const finalUrl = await ctx.proxiedFetcher<string>(`/playlist/${finalFilePath}.txt`, {
     baseUrl: mainUrl,
     method: 'POST',
