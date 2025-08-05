b9fe87fd70227c183898bbde484168d3a80920d5
Aykhan <ayxanshikhaliyevuv@gmail.com>
2025-08-05 11:36:30 +0400

    fix movies on vidsrc.wtf
    broke movie scraping while adding shows(

--- src/providers/sources/vidsrcwtf.ts
+++ src/providers/sources/vidsrcwtf.ts
@@ -5,7 +5,63 @@ import { NotFoundError } from '@/utils/errors';
 
 const mainUrl = 'https://dezzu370xol.com';
 
-async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promise<SourcererOutput> {
+async function scrapeMovie(ctx: MovieScrapeContext): Promise<SourcererOutput> {
+  const imdbId = ctx.media.imdbId;
+  if (!imdbId) throw new NotFoundError('IMDb ID not found');
+
+  const playerPage = await ctx.proxiedFetcher<string>(`/play/${imdbId}`, {
+    baseUrl: mainUrl,
+    headers: {
+      Referer: 'https://www.vidsrc.wtf/',
+    },
+  });
+
+  const playerConfigMatch = playerPage.match(/let pc = ({.*});/s);
+  if (!playerConfigMatch) throw new Error('Failed to find player config');
+  const playerConfig = JSON.parse(playerConfigMatch[1]);
+
+  const sources = await ctx.proxiedFetcher<any>(playerConfig.file, {
+    method: 'POST',
+    headers: {
+      'X-CSRF-TOKEN': playerConfig.key,
+      Referer: `${mainUrl}/`,
+      Origin: mainUrl,
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:141.0) Gecko/20100101 Firefox/141.0',
+      'Content-type': 'application/x-www-form-urlencoded',
+    },
+  });
+
+  const englishSource = sources[1];
+  if (!englishSource) throw new Error('No english source found');
+
+  const finalFilePath = englishSource.file.startsWith('~') ? englishSource.file.substring(1) : englishSource.file;
+  const finalUrl = await ctx.proxiedFetcher<string>(`/playlist/${finalFilePath}.txt`, {
+    baseUrl: mainUrl,
+    method: 'POST',
+    headers: {
+      'X-CSRF-TOKEN': playerConfig.key,
+      Referer: `${mainUrl}/play/${imdbId}`,
+      Origin: mainUrl,
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:141.0) Gecko/20100101 Firefox/141.0',
+      'Content-type': 'application/x-www-form-urlencoded',
+    },
+  });
+
+  return {
+    embeds: [],
+    stream: [
+      {
+        id: 'primary',
+        type: 'hls',
+        playlist: finalUrl,
+        flags: [],
+        captions: [],
+      },
+    ],
+  };
+}
+
+async function scrapeShow(ctx: ShowScrapeContext): Promise<SourcererOutput> {
   const imdbId = ctx.media.imdbId;
   if (!imdbId) throw new NotFoundError('IMDb ID not found');
 
@@ -38,24 +94,16 @@ async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promis
     },
   );
 
-  let finalFilePath: string;
+  const media = ctx.media as ShowMedia;
+  const season = sources.find((s: any) => s.title === `Season ${media.season.number}`);
+  if (!season) throw new NotFoundError('Season not found');
 
-  if (ctx.media.type === 'movie') {
-    const englishSource = sources[1];
-    if (!englishSource) throw new Error('No english source found');
-    finalFilePath = englishSource.file;
-  } else {
-    const media = ctx.media as ShowMedia;
-    const season = sources.find((s: any) => s.title === `Season ${media.season.number}`);
-    if (!season) throw new NotFoundError('Season not found');
+  const episode = season.folder.find((e: any) => e.episode === `${media.episode.number}`);
+  if (!episode) throw new NotFoundError('Episode not found');
 
-    const episode = season.folder.find((e: any) => e.episode === `${media.episode.number}`);
-    if (!episode) throw new NotFoundError('Episode not found');
-
-    const englishSource = episode.folder.find((f: any) => f.title === 'English');
-    if (!englishSource) throw new NotFoundError('No english source found for episode');
-    finalFilePath = englishSource.file;
-  }
+  const englishSource = episode.folder.find((f: any) => f.title === 'English');
+  if (!englishSource) throw new NotFoundError('No english source found for episode');
+  let finalFilePath = englishSource.file;
 
   if (finalFilePath.startsWith('~')) {
     finalFilePath = finalFilePath.substring(1);
@@ -92,6 +140,6 @@ export const vidsrcWTFScraper = makeSourcerer({
   name: 'VidSrc.WT',
   rank: 179,
   flags: [],
-  scrapeMovie: comboScraper,
-  scrapeShow: comboScraper,
+  scrapeMovie,
+  scrapeShow,
 });
